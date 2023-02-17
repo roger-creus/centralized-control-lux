664ccb57882f29f31807ae60e1dbc8654cecc7d7
roger-creus <creus99@gmail.com>
2023-02-16 19:05:49 -0500

    wip invalid action masks

--- src/invalid_action_masks.py
+++ src/invalid_action_masks.py
@@ -43,6 +43,239 @@ def get_factory_invalid_action_masks(envs, player):
 
     return action_mask
 
+def get_robot_invalid_action_masks_action_type(envs, player):
+    if player == "player_0":
+        action_mask = np.zeros((len(envs.envs), 48, 48, envs.single_action_space["robots"].nvec[1] + 1))
+        envs = envs.envs
+
+    if player == "player_1":
+        action_mask = np.zeros((1, 48, 48, envs.action_space["robots"].nvec[1] + 1))
+        envs = [envs]
+
+    for i in range(len(envs)):
+        env = envs[i]
+        game_state = env.env_.state
+
+        # start by assuming NOOP is valid everywhere
+        action_mask[i, :, :, 1] = 1
+
+        for unit_id in game_state.units[player]:
+            unit = game_state.units[player][unit_id]
+            x, y = unit.pos.x, unit.pos.y
+
+            # start by assuming all actions are legal where there is a unit
+            action_mask[i, x, y, :] = 1
+
+            current_power = unit.power
+
+            # only noop is valid
+            if current_power <= update_queue_cost:
+                action_mask[i, x, y, 2:] = 0
+
+            ###### MOVE ACTION TYPE ######
+            move_cost_up = move_cost(unit, game_state, 1)
+            can_move_up = True
+            if move_cost_up == 99999:
+                can_move_up = False
+
+            if current_power <= move_cost_up + update_queue_cost:
+                can_move_up = False
+
+            move_cost_right = move_cost(unit, game_state, 2)
+            can_move_right = True
+            if move_cost_right == 99999:
+                can_move_right = False
+
+            if current_power <= move_cost_right + update_queue_cost:
+                can_move_right = False
+
+            can_move_down = True
+            move_cost_down = move_cost(unit, game_state, 3)
+            if move_cost_down == 99999:
+                can_move_down = False
+            
+            if current_power <= move_cost_down + update_queue_cost:
+                can_move_down = False
+
+            can_move_left = True
+            move_cost_left = move_cost(unit, game_state, 4)
+            if move_cost_left == 99999:
+                can_move_left = False
+            
+            if current_power <= move_cost_left + update_queue_cost:
+                can_move_left = False
+
+            if not can_move_up and not can_move_down and not can_move_left and not can_move_right:
+                action_mask[i,x,y,2] = 0
+
+            ###### TRANSFER ACTION TYPE ######
+            factory_center = game_state.board.factory_occupancy_map[unit.pos.x][unit.pos.y]
+            can_transfer_center = True
+            if factory_center == -1:
+                can_transfer_center = False
+
+            factory_up = game_state.board.factory_occupancy_map[unit.pos.x][unit.pos.y - 1]
+            can_transfer_up = True
+            if factory_up == -1:
+                can_transfer_up = False
+            
+            factory_right = game_state.board.factory_occupancy_map[unit.pos.x + 1][unit.pos.y]
+            can_transfer_right = True
+            if factory_right == -1:
+                can_transfer_right = False
+
+            factory_down = game_state.board.factory_occupancy_map[unit.pos.x][unit.pos.y + 1]
+            can_transfer_down = True
+            if factory_down == -1:
+                can_transfer_down = False
+
+            factory_left = game_state.board.factory_occupancy_map[unit.pos.x - 1][unit.pos.y]
+            can_transfer_left = True
+            if factory_left == -1:
+                can_transfer_left = False
+
+            if not can_transfer_left and not can_transfer_up and not can_transfer_down and not can_transfer_right and not can_transfer_center:
+                action_mask[i,x,y,3] = 0
+                
+            ###### PICKUP AND DIG ACTION TYPE ######
+            if factory_center == -1:
+                action_mask[i,x,y,4] = 0
+            else:
+                action_mask[i,x,y,5] = 0
+            
+            ###### SELF DESTRUCT ACTION TYPE ######
+            if current_power <= 10 + update_queue_cost and unit.unit_type == luxai_unit.UnitType.LIGHT:
+                action_mask[i, x, y, 6] = 0
+            if current_power <= 100 + update_queue_cost and unit.unit_type == luxai_unit.UnitType.HEAVY:
+                action_mask[i, x, y, 6] = 0
+    
+            ###### SELF DESTRUCT ACTION TYPE ######
+            if (current_power - update_queue_cost) / unit.battery_capacity >= 0.75 * unit.battery_capacity:
+                action_mask[i, x, y, 7] = 0
+
+    return action_mask    
+
+
+def get_robot_invalid_action_masks_action_params(envs, player, action_types):
+    # the returned mask will be of shape (num_envs, map_size, map_size, total_unit_actions)
+    if player == "player_0":
+        action_mask = np.zeros((len(envs.envs), 48, 48, envs.single_action_space["robots"].nvec[2:].sum() + 1))
+        envs = envs.envs
+
+    if player == "player_1":
+        action_mask = np.zeros((1, 48, 48, envs.action_space["robots"].nvec[2:].sum() + 1))
+        envs = [envs]
+
+    for i in range(len(envs)):
+        env = envs[i]
+        game_state = env.env_.state
+
+        for unit_id in game_state.units[player]:
+            unit = game_state.units[player][unit_id]
+            x, y = unit.pos.x, unit.pos.y
+
+            current_power = unit.power
+            update_queue_cost = action_queue_cost(unit, env.env_.env_cfg)
+
+            action_type = action_types[i, x, y]
+
+            # selected type is NOOP
+            if action_type == 0:
+                action_mask[i, x, y, 1:] = 0
+
+
+            move_cost_up = move_cost(unit, game_state, 1)
+            can_move_up = True
+            # no map or enemy factory on top -> cannot move or cannot transfer up
+            if move_cost_up == 99999:
+                action_mask[i, x, y, 8] = 0
+                action_mask[i, x, y, 13] = 0
+                can_move_up = False
+
+            if current_power <= move_cost_up + update_queue_cost:
+                action_mask[i, x, y, 8] = 0
+                can_move_up = False
+
+
+            move_cost_right = move_cost(unit, game_state, 2)
+            can_move_right = True
+            # no map or enemy factory to the right -> cannot move or cannot transfer right
+            if move_cost_right == 99999:
+                action_mask[i, x, y, 9] = 0
+                action_mask[i, x, y, 14] = 0
+                can_move_right = False
+
+            if current_power <= move_cost_right + update_queue_cost:
+                action_mask[i, x, y, 9] = 0
+                can_move_right = False
+
+            can_move_down = True
+            move_cost_down = move_cost(unit, game_state, 3)
+            # no map or enemy factory down -> cannot move or cannot transfer down
+            if move_cost_down == 99999:
+                action_mask[i, x, y, 10] = 0
+                action_mask[i, x, y, 15] = 0
+                can_move_down = False
+            
+            if current_power <= move_cost_down + update_queue_cost:
+                action_mask[i, x, y, 10] = 0
+                can_move_down = False
+
+            can_move_left = True
+            move_cost_left = move_cost(unit, game_state, 4)
+            # no map or enemy factory left -> cannot move or cannot transfer left
+            if move_cost_left == 99999:
+                action_mask[i, x, y, 11] = 0
+                action_mask[i, x, y, 16] = 0
+                can_move_left = False
+            
+            if current_power <= move_cost_left + update_queue_cost:
+                action_mask[i, x, y, 11] = 0
+                can_move_left = False
+
+
+            # if cannot move in any direction then simply cannot move
+            if not can_move_up and not can_move_down and not can_move_left and not can_move_right:
+                action_mask[i,x,y,2] = 0
+
+            factory_there = game_state.board.factory_occupancy_map[unit.pos.x][unit.pos.y]
+
+            # if there is no factory on top of unit -> cannot pick up (no pick up amount [4 actions] and no pick up resource [5 actions])
+            if factory_there == -1:
+                action_mask[i, x, y, 4] = 0
+                action_mask[i, x, y, 26:35] = 0
+            else:
+                action_mask[i, x, y, 5] = 0
+
+            # mask dig if no power (light robot)
+            if current_power <= 5 + update_queue_cost and unit.unit_type == luxai_unit.UnitType.LIGHT:
+                action_mask[i, x, y, 5] = 0
+
+            # mask dig if no power (heavy robot)
+            if current_power <= 60  + update_queue_cost and unit.unit_type == luxai_unit.UnitType.HEAVY:
+                action_mask[i, x, y, 5] = 0
+
+            # mask self destruct if no power (light robot)
+            if current_power <= 10 + update_queue_cost and unit.unit_type == luxai_unit.UnitType.LIGHT:
+                action_mask[i, x, y, 6] = 0
+
+            # mask self destruct if no power (heavyrobot)
+            if current_power <= 100 + update_queue_cost and unit.unit_type == luxai_unit.UnitType.HEAVY:
+                action_mask[i, x, y, 6] = 0
+
+            # mask recharge if already have enough power
+            if (current_power - update_queue_cost) / unit.battery_capacity >= 0.25 * unit.battery_capacity:
+                action_mask[i, x, y, 35] = 0
+
+            if (current_power - update_queue_cost) / unit.battery_capacity >= 0.5 * unit.battery_capacity:
+                action_mask[i, x, y, 36] = 0
+
+            if (current_power - update_queue_cost) / unit.battery_capacity >= 0.75 * unit.battery_capacity:
+                action_mask[i, x, y, 37] = 0
+
+    return action_mask
+
+
 def get_robot_invalid_action_masks(envs, player):
     # the returned mask will be of shape (num_envs, map_size, map_size, total_unit_actions)
     if player == "player_0":
@@ -69,57 +302,94 @@ def get_robot_invalid_action_masks(envs, player):
             update_queue_cost = action_queue_cost(unit, env.env_.env_cfg)
 
             # if not enough power to update action queue then we cannot do anything but NOOP
-
             if current_power <= update_queue_cost:
                 action_mask[i, x, y, 2:] = 0
 
-            # TODO: if cannot move in any direction -> Action type: Move should be ilegal
+
+            ###### MOVE ACTION TYPE ######
             move_cost_up = move_cost(unit, game_state, 1)
-            
+            can_move_up = True
             # no map or enemy factory on top -> cannot move or cannot transfer up
             if move_cost_up == 99999:
                 action_mask[i, x, y, 8] = 0
                 action_mask[i, x, y, 13] = 0
-
+                can_move_up = False
             if current_power <= move_cost_up + update_queue_cost:
                 action_mask[i, x, y, 8] = 0
-            #else:
-            #    print(env.env_.state.real_env_steps,":",unit_id, "at", (unit.pos.x, unit.pos.y) , "with power:", unit.power, "moving up cuz move cost is",move_cost_up,"and update cost is", update_queue_cost)
-
+                can_move_up = False
 
             move_cost_right = move_cost(unit, game_state, 2)
+            can_move_right = True
             # no map or enemy factory to the right -> cannot move or cannot transfer right
             if move_cost_right == 99999:
                 action_mask[i, x, y, 9] = 0
                 action_mask[i, x, y, 14] = 0
-
+                can_move_right = False
             if current_power <= move_cost_right + update_queue_cost:
                 action_mask[i, x, y, 9] = 0
-            #else:
-            #    print(env.env_.state.real_env_steps,":",unit_id, "at", (unit.pos.x, unit.pos.y) , "with power:", unit.power, "moving right cuz move cost is",move_cost_right,"and update cost is", update_queue_cost)
+                can_move_right = False
 
+            can_move_down = True
             move_cost_down = move_cost(unit, game_state, 3)
             # no map or enemy factory down -> cannot move or cannot transfer down
             if move_cost_down == 99999:
                 action_mask[i, x, y, 10] = 0
                 action_mask[i, x, y, 15] = 0
-            
+                can_move_down = False
             if current_power <= move_cost_down + update_queue_cost:
                 action_mask[i, x, y, 10] = 0
-            #else:
-            #    print(env.env_.state.real_env_steps,":",unit_id, "at", (unit.pos.x, unit.pos.y) , "with power:", unit.power, "moving down cuz move cost is",move_cost_down,"and update cost is", update_queue_cost)
+                can_move_down = False
 
+            can_move_left = True
             move_cost_left = move_cost(unit, game_state, 4)
             # no map or enemy factory left -> cannot move or cannot transfer left
             if move_cost_left == 99999:
                 action_mask[i, x, y, 11] = 0
                 action_mask[i, x, y, 16] = 0
-            
+                can_move_left = False
             if current_power <= move_cost_left + update_queue_cost:
                 action_mask[i, x, y, 11] = 0
-            #else:
-            #    print(env.env_.state.real_env_steps,":",unit_id, "at", (unit.pos.x, unit.pos.y) , "with power:", unit.power, "moving left cuz move cost is",move_cost_left,"and update cost is", update_queue_cost)
+                can_move_left = False
 
+            # if cannot move in any direction then simply cannot move
+            if not can_move_up and not can_move_down and not can_move_left and not can_move_right:
+                action_mask[i,x,y,2] = 0
+
+            ###### TRANSFER ACTION TYPE ######
+            factory_center = game_state.board.factory_occupancy_map[unit.pos.x][unit.pos.y]
+            can_transfer_center = True
+            if factory_center == -1:
+                can_transfer_center = False
+                action_mask[i,x,y,12] = 0
+
+            factory_up = game_state.board.factory_occupancy_map[unit.pos.x][unit.pos.y - 1]
+            can_transfer_up = True
+            if factory_up == -1:
+                can_transfer_up = False
+                action_mask[i,x,y,13] = 0
+            
+            factory_right = game_state.board.factory_occupancy_map[unit.pos.x + 1][unit.pos.y]
+            can_transfer_right = True
+            if factory_right == -1:
+                can_transfer_right = False
+                action_mask[i,x,y,14] = 0
+
+            factory_down = game_state.board.factory_occupancy_map[unit.pos.x][unit.pos.y + 1]
+            can_transfer_down = True
+            if factory_down == -1:
+                can_transfer_down = False
+                action_mask[i,x,y,15] = 0
+
+            factory_left = game_state.board.factory_occupancy_map[unit.pos.x - 1][unit.pos.y]
+            can_transfer_left = True
+            if factory_left == -1:
+                can_transfer_left = False
+                action_mask[i,x,y,16] = 0
+
+            if not can_transfer_left and not can_transfer_up and not can_transfer_down and not can_transfer_right and not can_transfer_center:
+                action_mask[i,x,y,3] = 0
+
+            ###### TRANSFER ACTION TYPE ######
             factory_there = game_state.board.factory_occupancy_map[unit.pos.x][unit.pos.y]
 
             # if there is no factory on top of unit -> cannot pick up (no pick up amount [4 actions] and no pick up resource [5 actions])
